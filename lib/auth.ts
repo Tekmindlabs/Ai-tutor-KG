8e6ba2a2483950c654d1937793efcf7b5f773b22
Tekmindlabs <150348873+Tekmindlabs@users.noreply.github.com>
2024-12-29 17:11:20 +0500

    removed duplication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,11 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/lib/prisma"
-import { authConfig } from "./auth.config"
+import { authConfig } from "@/auth.config"
+import { Adapter } from "next-auth/adapters"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   session: { strategy: "jwt" },
   ...authConfig,
 })
